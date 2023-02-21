21bd4b3de6e8fc36701540a670fef78ceae3e5fd
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-21 15:20:58 -0800

    npm

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+// "test": "echo \"Error: no test specified\" && exit 1"
+
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => console.log('Example app listening on port 3'));
+
+console.log('Hello World!');
