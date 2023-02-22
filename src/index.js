b4a5d7ab928f19b34a7fced04494e4f5db32d63e
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-21 21:38:59 -0800

    babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 
+const sum = (a, b) => {
+    a + b
+}
 
-// "test": "echo \"Error: no test specified\" && exit 1"
+sum(1, 2)
 
 
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    getName() {
+        return this.name
+    }
+    setName(name) {
+        return this.name = name
+    }
+
+    
+ }
+
+
+ const person = new Person('John', 20)
